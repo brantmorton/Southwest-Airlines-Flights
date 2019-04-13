7b4fa3b42b961c44ab46ae9e32f8a0c396c2118a
root <root@LAPTOP-JEOQHU6P.localdomain>
2019-04-13 12:39:08 -0500

    this file will input search parameters for Southwest.py

---
+++ search_params.py
@@ -0,0 +1,6 @@
+# Input departure airport, departure date, and return date below. They must be in the same format as the strings
+# currently stored.
+
+departure_airport = 'HOU'
+departure_date = '2019-05-25'
+return_date = '2019-05-27'
